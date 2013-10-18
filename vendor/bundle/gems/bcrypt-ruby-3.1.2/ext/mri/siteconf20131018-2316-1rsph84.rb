d45bc33da49deaa464a7aadd3c5e6f3a1ee6db6e
yan_runchen <runchen.yan@drecom.co.jp>
2013-10-18 16:35:52 +0900

    create User model

---
+++ vendor/bundle/gems/bcrypt-ruby-3.1.2/ext/mri/siteconf20131018-2316-1rsph84.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131018-2316-12ti7sk"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
