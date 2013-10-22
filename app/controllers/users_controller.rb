61eb062fc3f5bf83f84792ad21af9a464aa69394
yan_runchen <runchen.yan@drecom.co.jp>
2013-10-22 17:54:39 +0900

    add user pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,9 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
 
   def index
-    @users = User.all
+    @users = User.all.page(params[:page]).per(10)
   end
-  
+
   def show
   	@user = User.find(params[:id])
   end
